1f3117a401a032043aa622c5b461b66d86390df8
Shakuntala Mitra <shakuntala.mitra@gmail.com>
2019-04-04 22:03:31 -0700

    Add files via upload

---
+++ in silico digests/mouse/mouse-digest.R
@@ -0,0 +1,37 @@
+### modified from R-bloggers.com
+### load necessary libraries
+library(Biostrings)
+library(BSgenome.Mmusculus.UCSC.mm10)  ## use genome of interest
+library(plyr)
+library(ggplot2)
+library(reshape2)
+library(scales)
+
+# Identify XhoI recognition sites for each chromosomal entry
+# Generate dataframe with length of XhoI digested fragments 
+mdf = data.frame();
+for (i in seq_along(Mmusculus)){
+  print(paste("Processing ", seqnames(Mmusculus)[i], sep = ""))
+  m <- matchPattern("GGATCC", Mmusculus[[i]]) ## insert your enzyme's restriction site!!
+  starts <- start(gaps(m))
+  ends <- end(gaps(m))
+  temp_df <- data.frame(start = starts-6, end = ends, chr = seqnames(Mmusculus)[i])
+  temp_df$start <- replace(temp_df$start, temp_df$start == -3, 0)
+  temp_df <- temp_df[c("chr", "start", "end")]
+  mdf <- rbind(mdf, temp_df)
+}
+
+# Extract digested fragment length
+# Plot fragments within desired size range
+
+mdf$width = mdf$end - mdf$start
+ml <- mdf[mdf$width>0 & mdf$width<45001,]
+counts <- ddply(ml, .(width), nrow)
+
+# plot the frequency of the fragment lengths (y-axis is logarithmic)
+
+plot <- ggplot(counts, aes(x=width, y=V1)) + geom_line()
+plot + scale_y_continuous(trans = log2_trans())
+
+# write the counts to a .csv file
+write.table(counts,"BamHI-45000bp.xlsx",sep="\t",row.names=FALSE)
