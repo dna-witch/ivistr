ec834fd77cd9ab77bacb18f90196e1f30bc758ae
Shakuntala Mitra <shakuntala.mitra@gmail.com>
2019-04-04 21:57:03 -0700

    Add files via upload

---
+++ in silico digests/human/human-digest.R
@@ -0,0 +1,37 @@
+### modified from R-bloggers.com
+### load necessary libraries
+library(Biostrings)
+library(BSgenome.Hsapiens.UCSC.hg19)  ## use genome of interest
+library(plyr)
+library(ggplot2)
+library(reshape2)
+library(scales)
+
+# Identify XhoI recognition sites for each chromosomal entry
+# Generate dataframe with length of XhoI digested fragments 
+mdf = data.frame();
+for (i in seq_along(Hsapiens)){
+  print(paste("Processing ", seqnames(Hsapiens)[i], sep = ""))
+  m <- matchPattern("TCTAGA", Hsapiens[[i]]) ## insert your enzyme's restriction site!!
+  starts <- start(gaps(m))
+  ends <- end(gaps(m))
+  temp_df <- data.frame(start = starts-6, end = ends, chr = seqnames(Hsapiens)[i])
+  temp_df$start <- replace(temp_df$start, temp_df$start == -3, 0)
+  temp_df <- temp_df[c("chr", "start", "end")]
+  mdf <- rbind(mdf, temp_df)
+}
+
+# Extract digested fragment length
+# Plot fragments within desired size range
+
+mdf$width = mdf$end - mdf$start
+ml <- mdf[mdf$width>0 & mdf$width<200001,]
+counts <- ddply(ml, .(width), nrow)
+
+# plot the frequency of the fragment lengths (y-axis is logarithmic)
+
+plot <- ggplot(counts, aes(x=width, y=V1)) + geom_line()
+plot + scale_y_continuous(trans = log2_trans())
+
+# write the counts to a .csv file
+write.csv(counts,"XbaI-200000bp-hg19.csv", row.names=FALSE)
